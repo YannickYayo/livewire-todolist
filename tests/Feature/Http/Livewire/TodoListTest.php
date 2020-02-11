1b9d4b00aa461f046e42fb38046459af05eb3a1e
Yannick LEONE <yannick.leone@gmail.com>
2020-02-11 16:04:53 +0100

    refactor: refactor TodoListTest
    *  refactor TodoListTest a bit

--- tests/Feature/Http/Livewire/TodoListTest.php
+++ tests/Feature/Http/Livewire/TodoListTest.php
@@ -4,14 +4,17 @@ namespace Tests\Feature\Http\Livewire;
 
 use App\Http\Livewire\TodoList;
 use App\Models\Todo;
+use Illuminate\Database\Eloquent\Collection;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\WithFaker;
+use Illuminate\Pagination\LengthAwarePaginator;
 use Livewire;
 use Tests\TestCase;
 
 class TodoListTest extends TestCase
 {
     use RefreshDatabase;
+    use WithFaker;
 
     /**
      * Pagination limit.
@@ -28,15 +31,17 @@ class TodoListTest extends TestCase
 
     public function test_can_see_todos(): void
     {
-        $response = $this->get('/');
+        $this->getTodos($this->paginationLimit);
 
+        $response = $this->get('/');
         $response->assertStatus(200);
         $response->assertSee('Todo List');
+        $response->assertViewIs('todo-list');
     }
 
     public function test_see_limit_todos_at_a_time_by_default(): void
     {
-        factory(Todo::class, $this->paginationLimit + 1)->create();
+        $this->getTodos($this->paginationLimit + 1);
 
         Livewire::test(TodoList::class)
             ->assertViewHas('todos', function ($todos) {
@@ -46,15 +51,17 @@ class TodoListTest extends TestCase
 
     public function test_can_add_a_todo(): void
     {
+        $newTodo = $this->faker->realText(40);
+
         Livewire::test(TodoList::class)
             ->assertViewHas('todos', function ($todos) {
                 return $todos->total() == 0;
             })
-            ->call('addTodo', 'New Todo')
+            ->call('addTodo', $newTodo)
             ->assertViewHas('todos', function ($todos) {
                 return $todos->total() == 1;
             })
-            ->assertSee('New Todo')
+            ->assertSee($newTodo)
             ->assertSee('wire:keydown.enter="addTodo');
 
         $this->assertEquals(1, Todo::all()->count());
@@ -62,8 +69,7 @@ class TodoListTest extends TestCase
 
     public function test_show_the_last_page_after_adding_a_todo_when_a_new_page_is_created(): void
     {
-        factory(Todo::class, 5)->create();
-        $todos = Todo::paginate($this->paginationLimit);
+        $todos = $this->getTodos($this->paginationLimit, true);
         $currentPage = 1;
 
         Livewire::test(TodoList::class)
@@ -79,8 +85,7 @@ class TodoListTest extends TestCase
     public function test_show_the_last_page_after_adding_a_todo_when_no_new_page_is_created(): void
     {
         $countTodos = 16;
-        factory(Todo::class, $countTodos)->create();
-        $todos = Todo::paginate($this->paginationLimit);
+        $todos = $this->getTodos($countTodos, true);
         $currentPage = 2;
         $lastPage = intval(ceil($countTodos / $this->paginationLimit));
 
@@ -97,8 +102,7 @@ class TodoListTest extends TestCase
     public function test_stay_on_the_same_page_after_adding_a_todo_when_number_of_items_is_under_limit(): void
     {
         $countTodos = 4;
-        factory(Todo::class, $countTodos)->create();
-        $todos = Todo::paginate($this->paginationLimit);
+        $todos = $this->getTodos($countTodos, true);
         $currentPage = 1;
 
         Livewire::test(TodoList::class)
@@ -113,8 +117,8 @@ class TodoListTest extends TestCase
 
     public function test_can_delete_a_todo(): void
     {
-        $todosCreated = factory(Todo::class, 5)->create();
-        $todoToDelete = $todosCreated->get(1);
+        $todosCreated = $this->getTodos($this->paginationLimit);
+        $todoToDelete = $todosCreated->where('id', rand(1, $this->paginationLimit))->first();
 
         Livewire::test(TodoList::class)
             ->assertViewHas('todos', function ($todos) use ($todosCreated) {
@@ -132,8 +136,8 @@ class TodoListTest extends TestCase
 
     public function test_go_to_previous_page_if_delete_last_todo_on_current_page(): void
     {
-        $todosCreated = factory(Todo::class, 6)->create();
-        $todoToDelete = $todosCreated->get(3);
+        $todosCreated = $this->getTodos($this->paginationLimit + 1);
+        $todoToDelete = $todosCreated->where('id', rand(1, $this->paginationLimit) + 1)->first();
         $currentPage = 2;
 
         Livewire::test(TodoList::class)
@@ -142,13 +146,16 @@ class TodoListTest extends TestCase
             ->assertSet('page', $currentPage - 1)
             ->assertViewHas('todos', function ($todos) use ($todosCreated) {
                 return $todos->total() == $todosCreated->count() - 1;
+            })
+            ->assertViewHas('todos', function ($todos) use ($todosCreated) {
+                return $todos->count() == $this->paginationLimit;
             });
     }
 
     public function test_stay_on_the_same_page_after_deleting_a_todo_if_items_still_present_on_the_current_page(): void
     {
-        $todosCreated = factory(Todo::class, 7)->create();
-        $todoToDelete = $todosCreated->get(2);
+        $todosCreated = $this->getTodos($this->paginationLimit + 2);
+        $todoToDelete = $todosCreated->where('id', rand(1, $this->paginationLimit) + 2)->first();
         $currentPage = 2;
 
         Livewire::test(TodoList::class)
@@ -157,6 +164,24 @@ class TodoListTest extends TestCase
             ->assertSet('page', $currentPage)
             ->assertViewHas('todos', function ($todos) use ($todosCreated) {
                 return $todos->total() == $todosCreated->count() - 1;
+            })
+            ->assertViewHas('todos', function ($todos) use ($todosCreated) {
+                return $todos->count() == 1;
             });
+    }
+
+    /**
+     * Generate and get paginated todos.
+     *
+     * @param int|null $number
+     * @param bool $paginated
+     *
+     * @return LengthAwarePaginator|Collection
+     */
+    private function getTodos(?int $number = null, bool $paginated = false)
+    {
+        $todos = ! is_null($number) ? factory(Todo::class, $number)->create() : factory(Todo::class)->create();
+
+        return ! $paginated ? $todos : Todo::paginate($this->paginationLimit);
     }
 }
